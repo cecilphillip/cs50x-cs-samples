cd3d4680893bbc23ae45f4dcc9d2a0d0ca6b5aa1
Cecil Phillip <cecilphillip@gmail.com>
2015-09-21 19:03:56 -0400

    adding week 4

---
+++ week4/CS50xWeek4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CS50xWeek4.Models;
+
+namespace CS50xWeek4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                foreach (var cust in ctx.Customers)
+                {
+                    Console.WriteLine("Customer Info: ");
+                    Console.WriteLine("Customer Name: " + cust.ContactName);
+                    Console.WriteLine("Customer Address: " + cust.Address);
+                    Console.WriteLine("Order Count: " + cust.Orders.Count);
+                    foreach (var order in cust.Orders)
+                    {
+                        Console.WriteLine("Order Date:" + order.OrderDate);
+                    }
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
