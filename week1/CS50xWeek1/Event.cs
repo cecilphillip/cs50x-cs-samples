0732b3c7c4bae53eeb4ded331b71153a6b9bd256
Cecil Phillip <cecilphillip@gmail.com>
2016-05-16 21:00:19 -0400

    added Event

---
+++ week1/CS50xWeek1/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS50xWeek1
+{
+    public class Event
+    {        
+        public string Name;
+        public DateTime Time;
+        public string Location;
+        public int MaxOccupancy;
+        public decimal Price;
+
+        public Event(string Name)
+        {
+            this.Name = Name;
+        }
+
+
+        public void Cancel()
+        {
+            NotifyPersons();
+        }
+
+        public void NotifyPersons()
+        {
+            
+
+        }
+
+
+    }
+}
